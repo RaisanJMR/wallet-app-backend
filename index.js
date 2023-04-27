8383de15417fd64c0ca85812b141c0e1724d3b52
RaisanJMR <raisanjmr@protonmail.com>
2023-04-27 11:24:23 +0530

    added cors

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const dotenv = require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors')
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorHandler')
-
 
 connectDB()
 
 const app = express()
-
+// Enable CORS
+app.use(cors())
+app.options('*', cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
